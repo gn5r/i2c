63eaa649b07f3e3d2a4d1dd29a95fcefffb37c0e
gn5r <shangyuan.tuolang@gmail.com>
2018-11-14 19:28:16 +0900

    MegaのアドレスをSample.javaから指定できるように変更

---
+++ src/main/java/hal/tokyo/i2c/i2c/ArduinoMega.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hal.tokyo.i2c;
+
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+import com.pi4j.io.i2c.I2CFactory;
+import java.io.IOException;
+
+/**
+ *
+ * @author gn5r
+ */
+public class ArduinoMega {
+
+
+    /*    I2C通信用のグローバル変数    */
+    private I2CBus bus;
+    private I2CDevice device;
+    
+    /*    スレーブレジスタアドレス    */
+    private int addr;
+    
+    /*    コンストラクタ    */
+    public ArduinoMega(int addr) throws I2CFactory.UnsupportedBusNumberException, IOException {
+        this.addr = addr;
+    }
+
+    /*    privateコンストラクタ    */
+    private ArduinoMega() throws I2CFactory.UnsupportedBusNumberException, IOException {
+        /*     ラズパイのI2C_BUS1に接続したと仮定    */
+        this.bus = I2CFactory.getInstance(I2CBus.BUS_1);
+
+        /*    ArduinoMegaのアドレスはターミナルから検索可能    */
+        this.device = bus.getDevice(addr);
+    }
+
+    /*    データ読み取りメソッド    */
+    public int read() throws IOException {
+        return this.device.read();
+    }
+}
---
+++ src/main/java/hal/tokyo/i2c/i2c/Sample.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hal.tokyo.i2c;
+
+/**
+ *
+ * @author gn5r
+ */
+public class Sample {
+
+    public static void main(String[] args) throws Exception {
+
+        /*    ArduinoMegaクラスのインスタンスを生成    */
+        ArduinoMega arduinoMega = new ArduinoMega(0x20);
+
+        /*    1秒間隔でI2Cデバイスから受信したデータを表示    */
+        while (true) {
+            System.out.println(String.valueOf(arduinoMega.read()));
+            Thread.sleep(1000);
+        }
+        
+    }
+}
